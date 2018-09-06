9055038bad6c27fce5a9f04a9351448ec2b26bed
Ashutosh <apandey.77@gmail.com>
2018-09-06 18:24:54 -0400

    Added debug files to dump logs

---
+++ include/pladapt/DebugFileInfo.h
@@ -0,0 +1,50 @@
+/*
+ * DebugFileInfo.h
+ *
+ *  Created on: Jul 18, 2018
+ *      Author: ashutosp
+ */
+
+#ifndef UTIL_DEBUGFILEINFO_H_
+#define UTIL_DEBUGFILEINFO_H_
+
+using namespace std;
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class DebugFileInfo {
+private:
+    static DebugFileInfo* mDebugFileInfo;
+    ofstream* mFoutP;
+    const int mSeed;
+    const string mParentDirectory;
+    string mMode;
+    const string mDetailedDebugFile;
+
+    DebugFileInfo(const int seed, const string& parentDirectory, const string& mode);
+    void CreateLogsDir() const;
+    string GetLogsDirPath() const;
+    string GetSeedDirPath() const;
+
+public:
+    static DebugFileInfo* getInstance(int seed = -1, const string& parentDir = "", const string& mode = "Fast") {
+        if (mDebugFileInfo == NULL) {
+            mDebugFileInfo = new DebugFileInfo(seed, parentDir, mode);
+        }
+
+        return mDebugFileInfo;
+    }
+
+    string GetDebugFilePath() const;
+
+    void write(const string& output) const;
+    void write(const int& output) const;
+    void closeWriter();
+
+    virtual ~DebugFileInfo();
+};
+
+#endif /* UTIL_DEBUGFILEINFO_H_ */
