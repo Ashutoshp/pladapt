ed82a795c07ae82cce29e20d9ecae5b2df4550cb
Ashutosh <apandey.77@gmail.com>
2018-09-07 12:15:54 -0400

    Changed file structure

---
+++ examples/dart/dartam/include/dartam/DebugFileInfo.h
@@ -0,0 +1,45 @@
+/*
+ * DebugFileInfo.h
+ *
+ *  Created on: Jul 18, 2018
+ *      Author: ashutosp
+ */
+
+#ifndef UTIL_DEBUGFILEINFO_H_
+#define UTIL_DEBUGFILEINFO_H_
+
+using namespace std;
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class DebugFileInfo {
+private:
+    static DebugFileInfo* mDebugFileInfo;
+    ofstream* mFoutP;
+    const int mSeed;
+    const string mParentDirectory;
+    string mMode;
+    const string mDetailedDebugFile;
+
+    DebugFileInfo(const int seed, const string& parentDirectory, const string& mode);
+    void CreateLogsDir() const;
+    string GetLogsDirPath() const;
+    string GetSeedDirPath() const;
+
+public:
+    static DebugFileInfo* getInstance(const int seed = -1,
+            const char* parentDir = "", const char* mode = "Fast");
+
+    string GetDebugFilePath() const;
+
+    void write(const string& output) const;
+    void write(const int& output) const;
+    void closeWriter();
+
+    virtual ~DebugFileInfo();
+};
+
+#endif /* UTIL_DEBUGFILEINFO_H_ */
---
+++ examples/dart/dartam/src/DebugFileInfo.cpp
@@ -0,0 +1,94 @@
+/*
+ * DebugFileInfo.cc
+ *
+ *  Created on: Jul 18, 2018
+ *      Author: ashutosp
+ */
+
+#include <assert.h>
+#include <boost/filesystem.hpp>
+#include <pladapt/DebugFileInfo.h>
+
+DebugFileInfo* DebugFileInfo::mDebugFileInfo = NULL;
+
+DebugFileInfo::DebugFileInfo(const int seed, const string& parentDirectory, const string& mode) :
+          mSeed(seed),
+          mParentDirectory(parentDirectory),
+          mMode(mode),
+          mDetailedDebugFile("debug") {
+    // TODO Auto-generated constructor stub
+
+    if (mode == "pg") mMode = "ProbGeneration";
+    else if (mode == "cb") mMode = "Hybrid";
+    else if (mode == "ml0") mMode = "Hybrid_IBL0";
+    else if (mode == "ml1") mMode = "Hybrid_IBL1";
+    else if (mode == "slow") mMode = "Slow";
+    else if (mode == "fast") mMode = "Fast";
+    else if (mode == "si") mMode = "SlowInstant";
+    else assert(false);
+
+    CreateLogsDir();
+    mFoutP = new ofstream(GetDebugFilePath().c_str());
+
+	if (mFoutP == NULL) {
+		cout << "Could not open log file  "
+				<< GetDebugFilePath() << endl;
+	}
+}
+
+DebugFileInfo::~DebugFileInfo() {
+    // TODO Auto-generated destructor stub
+	mFoutP->close();
+
+	if (mFoutP != NULL) delete mFoutP;
+	mDebugFileInfo = NULL;
+}
+
+DebugFileInfo* DebugFileInfo::getInstance(const int seed, const char* parentDir, const char* mode) {
+    if (mDebugFileInfo == NULL) {
+        mDebugFileInfo = new DebugFileInfo(seed, parentDir, mode);
+    }
+
+    return mDebugFileInfo;
+}
+
+void DebugFileInfo::write(const string& output) const {
+	*mFoutP << output << endl;
+}
+
+void DebugFileInfo::write(const int& output) const {
+	*mFoutP << output << endl;
+}
+
+void DebugFileInfo::closeWriter() {
+	delete mDebugFileInfo;
+}
+
+void DebugFileInfo::CreateLogsDir() const {
+    string dir = GetLogsDirPath();
+    boost::filesystem::path p(dir);
+    boost::system::error_code errCode;
+    
+    if (!(boost::filesystem::exists(p)
+            && boost::filesystem::is_directory(p))) {
+        bool result = boost::filesystem::create_directories(
+					dir, errCode);
+
+        assert(result);
+    }
+}
+
+string DebugFileInfo::GetLogsDirPath() const {
+    return GetSeedDirPath() + "/" + mMode;
+}
+
+string DebugFileInfo::GetSeedDirPath() const {
+    return mParentDirectory + "/" + std::to_string(mSeed);
+}
+
+string DebugFileInfo::GetDebugFilePath() const {
+    string path = "";
+    path = GetLogsDirPath() + "/" + mDetailedDebugFile;
+
+    return path;
+}
